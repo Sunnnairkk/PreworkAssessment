c358519f447261aee5299fc670d93c8f9f3dfc28
sunilkumarkk <sunnairkk@gmail.com>
2021-08-02 16:14:12 +0530

    Prework Assessment - Multiple Operations

--- MultipleOperations.java
+++ MultipleOperations.java
@@ -2,7 +2,7 @@
 package com.greatlearning.preworkassessment;
 
 //Sunil Kumar K K
-//Prework Assessment - Example for Multiple Operations
+//Prework Assessment - Multiple Operations
 //01.08.2021
 
 import java.util.Scanner;
@@ -13,85 +13,78 @@ public class MultipleOperations {
 		// TODO Auto-generated method stub
 		
 		int sel;
+		int num=0;
 		
 		Operations op = new Operations();
 	
 		do {
 			
-			System.out.println("Multiple Operations Example");
-			System.out.println("1. Addition");
-			System.out.println("2. Substration");
-			System.out.println("3. Multiplication");
-			System.out.println("4. Divisioin");
-			System.out.println("5. Remainder");
-			System.out.println();
-			System.out.println("0. Exit");
-			System.out.println();
-			System.out.println("Select an option to continue");
+			System.out.println("Multiple Operations\n" +
+					"1. Palindrome Number\n" +
+					"2. Pattern of Stars\n" +
+					"3. Prime Number\n" +
+					"4. Fibonacci Series\n" +
+					
+					"\n0. Exit\n\n" +
+					"Enter menu option\n");
 			Scanner input = new Scanner(System.in);
 			sel=input.nextInt();
 			
+			
 			switch (sel) {
 			case 1:
-				System.out.println("Addition Operation");
-				System.out.println("Enter First Number :");
-				op.num1=input.nextFloat();
-				System.out.println("Enter Second Number :");
-				op.num2=input.nextFloat();
-				System.out.println("Ans : " + op.addTwo());
-				System.out.println();
+				System.out.println("Checking for Palindrone Number\n" +
+						"Enter Number :");
+				num=input.nextInt();
+				if(num!=0) 
+					op.palindrome(num);
+				else
+					System.out.println("0 Entered\n");
 				break;
+				
 			case 2:
-				System.out.println("Substration Operation");
-				System.out.println("Enter First Number :");
-				op.num1=input.nextFloat();
-				System.out.println("Enter Second Number :");
-				op.num2=input.nextFloat();
-				System.out.println("Ans : " + op.subTwo());
-				System.out.println();
+				System.out.println("Pattern of Stars\n" + 
+						"Enter the Number of Stars : ");
+				num=input.nextInt();
+				if(num!=0) 
+					op.patternOfStars(num);
+				else
+					System.out.println("0 entered\n");
 				break;
+				
 			case 3:
-				System.out.println("Multiplication Operation");
-				System.out.println("Enter First Number :");
-				op.num1=input.nextFloat();
-				System.out.println("Enter Second Number :");
-				op.num2=input.nextFloat();
-				System.out.println("Ans : " + op.mulTwo());
-				System.out.println();
+				System.out.println("Enter a Number to check whether Prime :");
+				num=input.nextInt();
+				if (num!=0)
+					op.checkPrimeNumber(num);
+				else
+					System.out.println("0 entered\n");
 				break;
+				
 			case 4:
-				System.out.println("Division Operation");
-				System.out.println("Enter First Number :");
-				op.num1=input.nextFloat();
-				System.out.println("Enter Second Number :");
-				op.num2=input.nextFloat();
-				if(op.num2==0) {
-					System.out.println("Division by 0 - ERROR");
-					System.out.println();
-				}
-				else {
-				System.out.println("Ans : " + op.divTwo());
-				System.out.println();
-				}
+				System.out.println("Enter a Number to Display Fibanocci Series :");
+				num=input.nextInt();
+				if (num!=0) {
+					System.out.print("0 1");
+					Operations.fibonacciSeries(num-2);
+					System.out.println("\n\n");
+				} 
+				else
+					System.out.println("0\n");
 				break;
-			case 5:
-				System.out.println("Remainder");
-				System.out.println("Enter First Number :");
-				op.num1=input.nextFloat();
-				System.out.println("Enter Second Number :");
-				op.num2=input.nextFloat();
-				System.out.println("Ans : " + op.modVal());
-				System.out.println();
-				break;
+				
 			case 0:
+				input.close();
 				System.out.println("Execution Finished");
 				break;
 			default:
-				System.out.println("Wrong Selection");
-				System.out.println();
+				System.out.println("Wrong Selection\n\n");
 			}
 			
 		} while (sel != 0);
+		
 	}
+
+
 
 }
--- Operations.java
+++ Operations.java
@@ -2,9 +2,10 @@ package com.greatlearning.preworkassessment;
 
 public class Operations {
 
-	float num1;
-	float num2;
-	int sel;
+	private float num1;
+	private float num2;
+	private int num3;
+	private String s="";
 	
 	public float getNum1() {
 		return num1;
@@ -22,6 +23,21 @@ public class Operations {
 		this.num2 = num2;
 	}
 	
+	public int getNum3() {
+		return num3;
+	}
+	
+	public void setNum3(int num3) {
+		this.num3 = num3;
+	}
+	
+	public void setS(String s) {
+		this.s = s;
+	}
+	
+	public String getS() {
+		return s;
+	}
 	
 	public float addTwo() {
 		return  (num1 + num2);
@@ -41,5 +57,72 @@ public class Operations {
 	
 	public float modVal() {
 		return (num1%num2);
+	}
+	
+	public void palindrome(int n) {
+		
+		int oNum = n;
+		int rNum=0;
+		int r;
+		
+		while(oNum!=0) {
+			r = oNum % 10;
+			rNum = rNum * 10 + r;
+			oNum /= 10;
+		}
+		
+		System.out.println();
+		System.out.println("Reversed Number is : " + rNum);
+		
+		if(n == rNum) {
+			System.out.println("Number is a Palindrome Number\n\n");
+		}
+		else {
+			System.out.println("Number is Not a Palindrome Number\n\n");
+		}
+	}
+
+	public void patternOfStars(int n) {
+			//decreasing pattern
+		
+		for (int a=n; a>0; a--) {
+			for (int b=1; b<=a; b++) {
+				System.out.print('*');
+			}
+			System.out.println();
+		}
+		System.out.println("\n");
+	}
+	
+	public void checkPrimeNumber (int n) {
+		
+		boolean factors=false;
+		
+		for (int i=2;i<n;i++) {
+			if(n%i==0) 
+				factors=true;
+			else
+				factors=false;
+			
+			if(factors==true) break;
+		}
+		
+		if (factors==true)
+			System.out.println("The Number is NOT a Prime Number\n\n");
+		else
+			System.out.println("The Number is a Prime Number\n\n");
+	}
+	
+	public static int a=0,b=1,c=0;
+	
+	public static void fibonacciSeries(int n) {
+		if (n>0) {
+			c = a + b;
+			System.out.print(" " + c);
+			a = b;
+			b = c;
+			fibonacciSeries(n-1);
+		}
+		else n=0;
 	}
 }
